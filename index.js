3b2bb29ae13656fc3ec27cf4f3f2ac6d81859b90
Matt Dean <mdean400@gmail.com>
2022-11-15 22:06:08 -0700

    Parameterize store id

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const { executablePath } = require('puppeteer')
 
 async function main() {
-  const twelve = 'https://www.microcenter.com/product/641917/intel-core-i7-12700k-alder-lake-36ghz-twelve-core-lga-1700-boxed-processor-heatsink-not-included?storeid=181'
-  const thirteen = 'https://www.microcenter.com/product/652626/intel-core-i7-13700k-raptor-lake-34ghz-sixteen-core-lga-1700-boxed-processor-heatsink-not-included?storeid=181'
+  const STORE_ID = '181'
+  // const url = 'https://www.microcenter.com/product/641917/intel-core-i7-12700k-alder-lake-36ghz-twelve-core-lga-1700-boxed-processor-heatsink-not-included'
+  const url = 'https://www.microcenter.com/product/652626/intel-core-i7-13700k-raptor-lake-34ghz-sixteen-core-lga-1700-boxed-processor-heatsink-not-included'
 
-  const inStock = await isInStock(thirteen)
-  console.log('in stock', inStock)
+  const inStock = await isInStock(url, STORE_ID)
+  console.log('in stock:', inStock)
 }
 
-async function isInStock(url) {
+async function isInStock(url, storeId) {
   puppeteer.use(StealthPlugin())
 
   const browser = await puppeteer.launch({executablePath: executablePath()});
   const page = await browser.newPage();
-  page.setCookie({'name': 'storeSelected', 'value': '181', domain: '.microcenter.com', session: false, secure: true})
+  page.setCookie({'name': 'storeSelected', 'value': storeId, domain: '.microcenter.com', session: false, secure: true})
   page.setCookie({'name': 'myStore', 'value': 'true', domain: '.microcenter.com', session: true, secure: true})
 
   // Load micro center page
-  await page.goto(url);
+  await page.goto(`${url}?storeid=${storeId}`);
 
   // Wait for the results page to load and display the results.
   const inventorySelector = '#pnlInventory .inventory';
