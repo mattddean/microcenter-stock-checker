f48449596c8380f81968721d47d36bb593dbf903
Matt Dean <mdean400@gmail.com>
2022-11-15 22:01:51 -0700

    Use stealth plugin, set cookies to Denver store

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const { executablePath } = require('puppeteer')
 
-(async() => {
-  const browser = await puppeteer.launch();
+async function main() {
+  const twelve = 'https://www.microcenter.com/product/641917/intel-core-i7-12700k-alder-lake-36ghz-twelve-core-lga-1700-boxed-processor-heatsink-not-included?storeid=181'
+  const thirteen = 'https://www.microcenter.com/product/652626/intel-core-i7-13700k-raptor-lake-34ghz-sixteen-core-lga-1700-boxed-processor-heatsink-not-included?storeid=181'
+
+  const inStock = await isInStock(thirteen)
+  console.log('in stock', inStock)
+}
+
+async function isInStock(url) {
+  puppeteer.use(StealthPlugin())
+
+  const browser = await puppeteer.launch({executablePath: executablePath()});
   const page = await browser.newPage();
+  page.setCookie({'name': 'storeSelected', 'value': '181', domain: '.microcenter.com', session: false, secure: true})
+  page.setCookie({'name': 'myStore', 'value': 'true', domain: '.microcenter.com', session: true, secure: true})
 
   // Load micro center page
-  await page.goto('https://www.microcenter.com/product/511342/dual-geforce-rtx-2080-ti-overclocked-dual-fan-11gb-gddr6-pcie-video-card');
+  await page.goto(url);
 
   // Wait for the results page to load and display the results.
-  const inventorySelector = '#pnlInventory .inventory .inventoryCnt';
+  const inventorySelector = '#pnlInventory .inventory';
   await page.waitForSelector(inventorySelector);
 
-  const itemNameElement = await page.$('#details h1 span[itemprop="name"] span');
-  const inventoryElement = await page.$(inventorySelector);
-  const inventoryStatus = await page.evaluate(inventoryElement => inventoryElement.textContent, inventoryElement);
-  const itemName = await page.evaluate(itemNameElement => itemNameElement.textContent, itemNameElement);
-  console.log('Item Name  : ' + itemName);
-  console.log('Item Status: ' + inventoryStatus);
-  if (inventoryStatus !== 'Sold Out') {
-    console.log('Item is in stock.');
-    // Send webook or something
-  } else {
-    console.log('Item is not stock.');
-  }
+  const inventoryStatusElement = await page.$('#pnlInventory .inventory .inventoryCnt');
+
   await browser.close();
-})();
+
+  if (inventoryStatusElement) {
+    return true
+  } else {
+    return false
+  }  
+}
+
+main().catch(console.error)
